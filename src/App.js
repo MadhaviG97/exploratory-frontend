c1645e9614fafb4614e0bf44bbd875487669acbb
MadhaviG97 <47152272+MadhaviG97@users.noreply.github.com>
2020-04-21 20:06:27 +0530

    project import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import signUp from "./views/guest-user/sign-up.jsx";
 import fillProfile from "./views/researcher/fill-profile";
 import UserProfile from "./views/researcher/user-profile.jsx";
 import AboutUs from "./views/shared/about-us.jsx";
-import Forum from "./views/shared/public-forum"
+import Forum from "./views/shared/public-forum";
 import signIn from "./views/researcher/sign-in";
 import Auth from "./hoc/auth";
 import Navbar from "./components/Navbar/Navbar";
@@ -22,6 +22,8 @@ import ProjectFolder from "./views/research/fileFolder/ProjectFolder";
 import ProjectPublic from "./views/research/index/ViewResearchPublic";
 import ProjectPrivate from "./views/research/index/ViewResearchPrivate";
 import CreateProject from "./views/research/index/CreateResearch";
+
+import Project from "./views/research/index/ViewResearchPublic";
 
 function App() {
   return (
@@ -70,12 +72,24 @@ function App() {
               path="/project/MyProject/:id"
               component={Auth(ProjectPrivate, null)}
             />
-            <Route exact path="/document/projectfolder" component={Auth(ProjectFolder,null)} />
-            <Route exact path="/document/edit/:postId" component={Auth(EditPage,null)} />
-            <Route exact path="/document/editorblog" component={Auth(EditorBlog,null)} />
-            
-            <Route exact path="/project" component={Auth(Project,null)} />
-            <Route exact path="/forum" component={Auth(Forum,true)} />
+            <Route
+              exact
+              path="/document/projectfolder"
+              component={Auth(ProjectFolder, null)}
+            />
+            <Route
+              exact
+              path="/document/edit/:postId"
+              component={Auth(EditPage, null)}
+            />
+            <Route
+              exact
+              path="/document/editorblog"
+              component={Auth(EditorBlog, null)}
+            />
+
+            <Route exact path="/project" component={Auth(Project, null)} />
+            <Route exact path="/forum" component={Auth(Forum, true)} />
             {/* <Route exact path="/modal" component={Modal} /> */}
           </Switch>
         </div>
