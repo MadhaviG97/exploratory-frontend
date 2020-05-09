c4b3360b9fd358085d0177df403ca6981836fa00
Damika Gamlath <damika97.10@gmail.com>
2020-05-09 09:23:25 +0530

    chat alignment

--- src/views/researcher/chat.jsx
+++ src/views/researcher/chat.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import {Grid , Paper} from '@material-ui/core'
+import { Grid, Paper } from '@material-ui/core'
 import Chat from '../../components/Chat/Chat'
 
 export default function ChatApp() {
   return (
-    <div className="App" style={{height:'100vh'}}>
-    <Grid container style={{height:'100%'}}>
-    <Paper style={{height:'100%'}}>
-    {/* <ChatList /> */}
-    <Chat/>
-    </Paper>
-    </Grid>
-  </div>
+    <div className="App" style={{ height: '100vh' }}>
+      <Grid
+        container
+        justify="flex-end"
+        alignItems="center"
+        style={{ height: '100%' }}>
+          <Paper style={{ height: '100%' }}>
+            {/* <ChatList /> */}
+            <Chat />
+          </Paper>
+      </Grid>
+    </div>
   );
 }
 
