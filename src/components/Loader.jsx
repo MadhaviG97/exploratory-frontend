653c866fe08f7a0c48b9db1dc525728aad7a5a06
Yogya <yogyagamage@gmail.com>
2020-06-07 14:38:10 +0530

    change loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,14 +2,23 @@
 import React from "react";
 import { Circle } from "react-spinners-css";
 import Grid from "@material-ui/core/Grid";
+import ReactLoading from "react-loading";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from '@material-ui/core/Box';
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "80vh",
+    height: "82vh",
+    alignItems: "center",
   },
   box: {
     alignItems: "center",
+  },
+  loader: {
+    height: 550,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
   },
 }));
 
@@ -19,23 +28,21 @@ export default function Loader() {
     <div>
       <Grid container component="main" className={classes.root}>
         {/* <CssBaseline /> */}
-        <Grid item xs={false} sm={5} md={5} />
-        <Grid item xs={12} sm={2} md={2} component="paper" elevation={6} square>
-          <Grid container>
-            <Grid item>
-              <Circle color="#014F82" size={150} />
-            </Grid>
-            <Grid item>
-              <Circle color="#014F82" size={150} />
-            </Grid>
-            <Grid item>
-              <Circle color="#014F82" size={150} />
+        
+        
+          <Grid container direction="row" justify="center"  alignItems="center">
+            <Grid item item align="center">
+              <div className={classes.loader}>
+                <ReactLoading
+                  type="spinningBubbles"
+                  color="#5054CC"
+                  height={550}
+                  width={50}
+                />
+              </div>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={false} sm={5} md={5} />
-      </Grid>
-      <Box p={5}/>
     </div>
   );
 }
